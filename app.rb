39aa16ed8ba9c73e1ed6e324dcd4e70ec43af3e9
Brett Karlson <brettjkarlson@gmail.com>
2021-07-14 15:55:31 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+ 
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "This is the secret route!"
+end
+
+@name =  %w(Amigo Misty Almond Oscar).sample
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+  end
+
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+  end
+
+get '/cat-form' do
+  erb(:cat_form)
+end
